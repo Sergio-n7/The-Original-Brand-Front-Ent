8a02095324e3ffdfb0a3095faa9add3f831e59b7
Sergio Salguero <salgueroflor1982@gmail.com>
2021-06-17 19:27:24 +0200

    creating Redux Actions

---
+++ src/redux/actions/garmetActions.ts
@@ -0,0 +1,183 @@
+import { Dispatch } from "redux";
+import axios from "axios";
+
+import {
+  FETCH_GARMETS_SUCCESS,
+  FETCH_GARMETS_ERROR,
+  FETCH_GARMET_DETAILS_SUCCESS,
+  FETCH_GARMET_DETAILS_ERROR,
+  FILTER_GARMET,
+  HANDLE_SEARCH_GARMET,
+  CREATE_GARMET_ERROR,
+  DELETE_GARMET_ERROR,
+  EDIT_GARMET_ERROR,
+  AllGarmetsActions,
+  Garmet,
+} from "../types/types";
+
+export const fetchGarmetsSucces = (garmets: Garmet[]): AllGarmetsActions => {
+  return {
+    type: FETCH_GARMETS_SUCCESS,
+    payload: garmets,
+  };
+};
+
+export const fetchGarmetsError = (error: Error | null): AllGarmetsActions => {
+  return {
+    type: FETCH_GARMETS_ERROR,
+    payload: error,
+  };
+};
+
+export const fetchGarmetDetailsSuccess = (
+  garmet: Garmet
+): AllGarmetsActions => {
+  return {
+    type: FETCH_GARMET_DETAILS_SUCCESS,
+    payload: garmet,
+  };
+};
+
+export const fetchGarmetDetailsError = (
+  error: Error | null
+): AllGarmetsActions => {
+  return {
+    type: FETCH_GARMET_DETAILS_ERROR,
+    payload: error,
+  };
+};
+
+export const filterGarmet = (garmets: Garmet[]): AllGarmetsActions => {
+  return {
+    type: FILTER_GARMET,
+    payload: garmets,
+  };
+};
+
+export const handleSearchGarmet = (searchField: string): AllGarmetsActions => {
+  return {
+    type: HANDLE_SEARCH_GARMET,
+    payload: searchField,
+  };
+};
+
+export const createGarmetError = (error: Error | null): AllGarmetsActions => {
+  return {
+    type: CREATE_GARMET_ERROR,
+    payload: error,
+  };
+};
+
+export const editGarmetError = (error: Error | null): AllGarmetsActions => {
+  return {
+    type: EDIT_GARMET_ERROR,
+    payload: error,
+  };
+};
+
+export const deleteGarmetError = (error: Error | null): AllGarmetsActions => {
+  return {
+    type: DELETE_GARMET_ERROR,
+    payload: error,
+  };
+};
+
+export const fechtGarmets = () => async (dispatch: Dispatch) => {
+  try {
+    const { data } = await axios.get("<urlFromHeroku>/garmets");
+    dispatch(fetchGarmetsSucces(data));
+    dispatch(filterGarmet(data));
+  } catch (error) {
+    dispatch(fetchGarmetsError(error));
+  }
+};
+
+export const fetchGarmetsDetails =
+  (garmetId: string) => async (dispatch: Dispatch) => {
+    try {
+      const { data } = await axios.get(`<urlFromHeroku>/garmets/${garmetId}`);
+      dispatch(fetchGarmetDetailsSuccess(data));
+    } catch (error) {
+      dispatch(fetchGarmetDetailsError(error));
+    }
+  };
+
+export const createGarmet = (form: FormData) => (dispatch: Dispatch) => {
+  try {
+    const temp = JSON.parse(localStorage.getItem("userInfo") as string);
+    const token = temp.token;
+    axios.post("<urlFromHeroku>/garmets", form, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "multipart/form-data",
+      },
+    });
+  } catch (error) {
+    dispatch(createGarmetError(error));
+  }
+};
+
+export const editGarmet =
+  (
+    name: string,
+    description: string,
+    category: string,
+    stock: number,
+    price: number,
+    color: string,
+    size: string,
+    garmetId: string
+  ) =>
+  (dispatch: Dispatch) => {
+    try {
+      const temp = JSON.parse(localStorage.getItem("userInfo") as string);
+      const token = temp.token;
+
+      axios.put(
+        `<urlFromHeroku>/garmets/${garmetId}`,
+        { name, description, category, stock, price, color, size },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+    } catch (error) {
+      dispatch(editGarmetError(error));
+    }
+  };
+
+export const deleteGarmet = (garmetId: string) => (dispatch: Dispatch) => {
+  try {
+    const temp = JSON.parse(localStorage.getItem("userInfo") as string);
+    const token = temp.token;
+
+    axios.delete(`<urlFromHeroku>/garmets/${garmetId}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  } catch (error) {
+    dispatch(deleteGarmetError(error));
+  }
+};
+
+type ReviewInput = {
+  name: string;
+  comment: string;
+  rating: number;
+};
+
+export const postReview =
+  (garmetId: string, review: ReviewInput) => async (dispatch: Dispatch) => {
+    const { name, comment, rating } = review;
+    try {
+      axios.put(`<urlFromHeroku>/garmets/review/${garmetId}`, {
+        name: name,
+        comment: comment,
+        rating: +rating,
+      });
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  };
---
+++ src/redux/actions/userActions.ts
@@ -0,0 +1,157 @@
+import { Dispatch } from "redux";
+import axios from "axios";
+
+import {
+  USER_SIGNIN_SUCCESS,
+  USER_SIGNIN_ERROR,
+  USER_SIGNOUT,
+  USER_REGISTER_SUCCESS,
+  USER_REGISTER_ERROR,
+  USER_EDIT_SUCCESS,
+  USER_EDIT_ERROR,
+  GET_ALL_USERS,
+  User,
+  AllUsersActions,
+} from "../types/types";
+
+export const siginSucces = (user: Partial<User>): AllUsersActions => {
+  return {
+    type: USER_SIGNIN_SUCCESS,
+    payload: user,
+  };
+};
+
+export const siginError = (error: Error | null): AllUsersActions => {
+  return {
+    type: USER_SIGNIN_ERROR,
+    paylad: error,
+  };
+};
+
+export const UserSignOut = () => {
+  return {
+    type: USER_SIGNOUT,
+  };
+};
+
+export const UserRegisterSucces = (user: User): AllUsersActions => {
+  return {
+    type: USER_REGISTER_SUCCESS,
+    payload: user,
+  };
+};
+
+export const UserRegisterError = (error: Error | null): AllUsersActions => {
+  return {
+    type: USER_REGISTER_ERROR,
+    payload: error,
+  };
+};
+
+export const userEditSucces = (user: User): AllUsersActions => {
+  return {
+    type: USER_EDIT_SUCCESS,
+    payload: user,
+  };
+};
+
+export const userEditError = (error: Error): AllUsersActions => {
+  return {
+    type: USER_EDIT_ERROR,
+    payload: error,
+  };
+};
+
+export const getAllUsers = (user: User[]): AllUsersActions => {
+  return {
+    type: GET_ALL_USERS,
+    payload: user,
+  };
+};
+
+export const fetchAllUsers = () => async (dispatch: Dispatch) => {
+  try {
+    const { data } = await axios.get(
+      "<url from heroku back-end deployment>/users"
+    );
+    dispatch(getAllUsers(data));
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+export const editUsers =
+  (
+    userId: string,
+    firstName: string,
+    lastName: string,
+    email: string,
+    age: number,
+    password: string,
+    isAdmin: boolean
+  ) =>
+  async (dispatch: Dispatch) => {
+    try {
+      const { data } = await axios.put(
+        `<url from heroku back-end deployment>/${userId}`,
+        {
+          firstName,
+          lastName,
+          email,
+          age,
+          password,
+          isAdmin,
+        }
+      );
+      dispatch(userEditSucces(data));
+    } catch (error) {
+      dispatch(userEditError(error));
+    }
+  };
+
+export const signin =
+  (email: string, password: string) => async (dispatch: Dispatch) => {
+    try {
+      const { data } = await axios.post(
+        "<url from heroku back-end deployment>/users",
+        { email, password }
+      );
+      dispatch(siginSucces(data));
+      localStorage.setItem("userInfo", JSON.stringify(data));
+    } catch (error) {
+      dispatch(siginError(error));
+    }
+  };
+
+export const register =
+  (
+    firstName: string,
+    lastName: string,
+    email: string,
+    age: number,
+    password: string
+  ) =>
+  async (dispatch: Dispatch) => {
+    try {
+      const { data } = await axios.post(
+        "<url from heroku back-end deployment>/users",
+        {
+          firstName,
+          lastName,
+          email,
+          age,
+          password,
+        }
+      );
+      dispatch(UserRegisterSucces(data));
+      dispatch(siginSucces(data));
+      localStorage.setItem("userInfo", JSON.stringify(data));
+    } catch (error) {
+      dispatch(UserRegisterError(error));
+    }
+  };
+
+export const signOut = () => async (dispatch: Dispatch) => {
+  localStorage.removeItem("userInfo");
+  dispatch(UserSignOut());
+};
